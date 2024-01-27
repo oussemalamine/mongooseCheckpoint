ad2f010af5e41ab637c2e0674b8b62f9b1052038
oussama <oussema.lamine20@gmail.com>
2024-01-27 17:16:52 +0100

    adding the auth to the prev app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = require('./config/connectDB');
 const dotenv = require('dotenv');
 const peopleRoutes = require('./routes/peopleRoutes');
+const authRoutes = require('./routes/auth');
+const protectedRoute = require('./routes/protectedRoute');
 
 dotenv.config();
 const app = express();
@@ -13,6 +15,8 @@ connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/protected', protectedRoute);
 
 // Mount people routes
 app.use('/people', peopleRoutes);
--- config/connectDB.js
+++ config/connectDB.js
@@ -3,14 +3,12 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            // Remove the useNewUrlParser and useUnifiedTopology options
-            // They are no longer needed in recent versions of the MongoDB driver
-        });
+        
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to the database');
     } catch (error) {
         console.error('Error connecting to the database:', error);
-        process.exit(1); // Exit the process if unable to connect
+        
     }
 };
 
